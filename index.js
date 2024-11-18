3425cf2de102bf0d351f33e43ecf9b5464f7c315
Sarfaraz-Hussain <call2me786@gmail.com>
2024-11-18 14:45:07 +0530

    added code

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const { resolve } = require('path');
-
 const app = express();
 const port = 3010;
 
+let cors = require('cors');
+let sqlite3 = require('sqlite3').verbose();
+let { open } = require('sqlite')
+app.use(cors);
+app.use(express.json());
+
+
+let db;
+(async () => {
+  db = await open({
+    filename: "./database.sqlite",
+    driver: sqlite3.Database,
+  });
+})();
+
+// ===========================================================
+async function fetchAllMovies(){
+  let query = "SELECT * FROM movies";
+  let response = await db.all(query, []);
+  return {movies: response};
+}
+app.get('/movies', async (req, res) => {
+  let results = await fetchAllMovies();
+  res.status(200).json(results);
+});
 
 
 app.listen(port, () => {
